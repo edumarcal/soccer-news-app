22c547bfd9eb6a470612cdd6e256230fc6ccdaca
Eduardo Marçal Oliveira <edumarcalnet@gmail.com>
2022-08-05 22:42:59 -0300

    Fix warning room schema export directory

--- app/src/main/java/me/dio/soccernews/data/local/SoccerNewsDb.java
+++ app/src/main/java/me/dio/soccernews/data/local/SoccerNewsDb.java
@@ -5,7 +5,7 @@ import androidx.room.RoomDatabase;
 
 import me.dio.soccernews.domain.News;
 
-@Database(entities = {News.class}, version = 1)
+@Database(entities = {News.class}, version = 1, exportSchema = false)
 public abstract class SoccerNewsDb extends RoomDatabase {
     public abstract NewsDao newsDao();
 }
